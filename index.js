9489015e2ec1a0ed4ce27cce3228efe68ee6300b
Jason Baik <baikjs87@gmail.com>
2022-05-05 23:07:16 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const userData = {
+        name: userName,
+        email: userEmail
+    }
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then(function(result) {
+        const p = document.createElement('p')
+        p.innerHTML = result.id
+        document.querySelector('body').appendChild(p)
+    })
+    .catch(function(error) {
+        const p = document.createElement('p')
+        p.innerHTML = error.message
+        document.querySelector('body').appendChild(p)
+    })
+}
